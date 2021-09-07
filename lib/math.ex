0aadbe0de4e10da5bcaf608c8cb31c0c981726f0
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-07 18:59:28 -0500

    Add Geomtry module

--- lib/math.ex
+++ lib/math.ex
@@ -4,4 +4,9 @@ defmodule Math do
   end
 
   def sub(a, b), do: a - b
+
+  defmodule Geometry do
+    def square_area(a), do: rectangle_area(a, a)
+    def rectangle_area(a, b), do: a * b
+  end
 end

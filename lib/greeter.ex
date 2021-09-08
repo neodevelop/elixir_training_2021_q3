e872c4cb91ea4b1e9a89c08dc24ac88a3a447a1e
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-07 19:29:30 -0500

    Add Hello with Pattern matching

--- lib/greeter.ex
+++ lib/greeter.ex
@@ -1,6 +1,7 @@
 defmodule Greeter do
   def hello(), do: "#{phrase()} anon!"
-  def hello(name), do: "Hello " <> name
+  # def hello(name), do: "Hello " <> name
+  def hello(%{name: name}), do: "Hello #{name}"
   def hello(name, lastname), do: "Hello #{name} Family #{lastname}"
 
   defp phrase(), do: "Whats'up "

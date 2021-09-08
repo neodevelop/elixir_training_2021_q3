f478b71468819cc201ec502dce2880c9058a7acc
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-07 19:30:14 -0500

    Agregao función área con pattern matching

--- lib/math.ex
+++ lib/math.ex
@@ -6,7 +6,22 @@ defmodule Math do
   def sub(a, b), do: a - b
 
   defmodule Geometry do
-    def square_area(a), do: rectangle_area(a, a)
-    def rectangle_area(a, b), do: a * b
+    # def square_area(a), do: rectangle_area(a, a)
+    # def rectangle_area(a, b), do: a * b
+    def area({:rectangle, a, b}) do
+      a * b
+    end
+
+    def area({:square, a}) do
+      a * a
+    end
+
+    def area({:circle, r}) do
+      r * r * 3.14
+    end
+
+    def area(_) do
+      :unsupported
+    end
   end
 end

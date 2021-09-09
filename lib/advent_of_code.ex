e90a1dbe3e86be8746e00f6974ec5bb9a6ba4576
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-09 18:40:22 -0500

    Nombrando la funcion de evaluación

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -1,14 +1,12 @@
 defmodule AdventOfCode do
   def get_floor(instructions) do
-    eval = fn
-      "(" -> 1
-      ")" -> -1
-      _ -> 0
-    end
-
     instructions
     |> String.split("")
-    |> Enum.map(eval)
+    |> Enum.map(&eval/1)
     |> Enum.sum()
   end
+
+  defp eval("("), do: 1
+  defp eval(")"), do: -1
+  defp eval(_), do: 0
 end

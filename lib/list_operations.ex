7a4e84b3edc2d6106a42280f96ee75fb74b7849a
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-09 18:48:13 -0500

    Impls la función map

--- lib/list_operations.ex
+++ lib/list_operations.ex
@@ -6,6 +6,16 @@ defmodule ListOperations do
   defp sum([], n), do: n
   defp sum([h | t], n), do: sum(t, n + h)
 
+  def my_map(list, function) do
+    exec_my_map(list, function, [])
+  end
+
+  defp exec_my_map([], _function, res), do: res
+
+  defp exec_my_map([h | t], function, res) do
+    exec_my_map(t, function, res ++ [function.(h)])
+  end
+
   # Obtener la longitud de una lista
   # Obtener el máximo común divisor
 end

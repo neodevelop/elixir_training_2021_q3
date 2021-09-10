c671bf27f94f63917c9fe83afc6ee6fb7d30e6e2
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-09 19:01:13 -0500

    Uso alias e import

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -1,9 +1,12 @@
 defmodule AdventOfCode do
   def get_floor(instructions) do
+    alias String, as: S
+    import Enum, only: [map: 2, sum: 1]
+
     instructions
-    |> String.split("")
-    |> Enum.map(&eval/1)
-    |> Enum.sum()
+    |> S.split("")
+    |> map(&eval/1)
+    |> sum()
   end
 
   defp eval("("), do: 1

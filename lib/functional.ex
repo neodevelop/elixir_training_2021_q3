8a99fcf03c59da82c04b01941ebab7afa1d313ee
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-13 17:52:48 -0500

    Agrego el primer principio funcional

---
+++ lib/functional.ex
@@ -0,0 +1,11 @@
+defmodule Functional do
+  def unique_letters(name) do
+    "Hello " <> name
+  end
+
+  def ask_and_respond() do
+    name = IO.gets("Cuál es tu nombre? ")
+    IO.puts(unique_letters(name))
+    :ok
+  end
+end

6b62b0410fe99e6ad3746fee822316bf24ac6048
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-13 17:22:06 -0500

    Complete Advent of code day 1ç

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -2,6 +2,7 @@ defmodule AdventOfCode do
   def get_floor(instructions) do
     alias String, as: S
     import Enum, only: [map: 2, sum: 1]
+    # import Enum, except: [map: 2, sum: 1]
 
     instructions
     |> S.split("")
@@ -9,7 +10,22 @@ defmodule AdventOfCode do
     |> sum()
   end
 
+  def get_floor_v2(instructions) do
+    instructions
+    |> String.split("")
+    |> Enum.map(&eval_2/1)
+    |> Enum.sum()
+  end
+
   defp eval("("), do: 1
   defp eval(")"), do: -1
   defp eval(_), do: 0
+
+  defp eval_2(char) do
+    cond do
+      char == "(" -> 1
+      char == ")" -> -1
+      true -> 0
+    end
+  end
 end

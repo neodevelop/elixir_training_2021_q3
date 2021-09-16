55e5c006d7f0ee917bc0d60852c6951386efcf41
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-16 18:24:55 -0500

    Haciendo el proceso persistente

--- lib/fibonacci_server.ex
+++ lib/fibonacci_server.ex
@@ -1,10 +1,14 @@
 defmodule FibonacciServer do
   def start() do
-    spawn(fn ->
-      receive do
-        {:compute, n} -> IO.puts(Fibonacci.sequence(n))
-        _ -> IO.puts("Error")
-      end
-    end)
+    spawn(FibonacciServer, :loop, [])
+  end
+
+  def loop() do
+    receive do
+      {:compute, n} -> IO.puts(Fibonacci.sequence(n))
+      _ -> IO.puts("Error")
+    end
+
+    loop()
   end
 end

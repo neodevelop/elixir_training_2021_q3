cc02a79de8ee879da53fdb09823eeb5019659790
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-16 19:15:20 -0500

    handle_message en factorial sin receive

--- lib/factorial.ex
+++ lib/factorial.ex
@@ -3,18 +3,11 @@ defmodule Factorial do
   def of(n) when is_number(n) and n > 0, do: n * of(n - 1)
   def of(_), do: {:error, :invalid}
 
-  def handle_message(parent) do
-    receive do
-      {:compute, n} ->
-        send(parent, {:ok, of(n)})
-        handle_message(parent)
+  def handle_message({:compute, n}, parent) do
+    send(parent, {:ok, of(n)})
+  end
 
-      :kill ->
-        send(parent, {:ok, :killed})
-
-      _ ->
-        send(parent, {:error, :invalid})
-        handle_message(parent)
-    end
+  def handle_message(_, parent) do
+    send(parent, {:error, :invalid})
   end
 end

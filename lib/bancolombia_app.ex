6cb1020de03dccd1b60ae2ab559d701eb7ce6d9d
Jose Juan Reyes <neodevelop@gmail.com>
2021-09-20 19:15:37 -0500

    Regreso el módulo principal

---
+++ lib/bancolombia_app.ex
@@ -0,0 +1,18 @@
+defmodule BancolombiaApp do
+  @moduledoc """
+  Documentation for `BancolombiaApp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BancolombiaApp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
